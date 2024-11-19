a7c46902e4f9dcec1d0f3f9ff88afc53feaac02b
Panosso <felipepanosso21@gmail.com>
2024-11-19 00:43:00 -0300

    v0.0.4

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/FelipePn10/api-golang/router"
+import (
+	"github.com/FelipePn10/api-golang/config"
+	"github.com/FelipePn10/api-golang/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error:: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
